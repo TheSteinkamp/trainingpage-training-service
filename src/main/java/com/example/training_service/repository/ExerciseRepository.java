f8b5eab05792f8ff63f6b946f6b3b8b09de4199a
Jessica Steinkamp <jessica@steinkamp.se>
2026-04-25 08:48:15 +0200

    edited yaml file

--- src/main/java/com/example/training_service/repository/ExerciseRepository.java
+++ src/main/java/com/example/training_service/repository/ExerciseRepository.java
@@ -4,10 +4,12 @@ import com.example.training_service.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     List<Exercise> getExercisesByBodyPart(@PathVariable String bodypart);
 
